de83be6692ee2b242b7e5bd6eeab30c9d3ec9bfb
Abdo <abdelatif.azdoud@gmail.com>
2021-03-18 12:24:59 +0100

    Updating the appBar

--- app/src/main/java/com/example/dashboarddesign/MainActivity.java
+++ app/src/main/java/com/example/dashboarddesign/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.dashboarddesign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+
+import com.google.android.material.appbar.AppBarLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppBarLayout layout = (AppBarLayout) findViewById(R.id.appBarLayout);
+        layout.setOutlineProvider(null);
+
+        /*DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        if (!drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.openDrawer(GravityCompat.START);
+        } else {
+            drawer.closeDrawer(GravityCompat.START);
+        }*/
+
     }
 }
